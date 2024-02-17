e0d485628c2af5ce859995e713539a9d98483bce
ByeolNabi <20183338@office.deu.ac.kr>
2024-02-17 17:44:26 +0900

    csv파일을 어떻게 다루어야 좋을까 잠깐 생각해봤는데요... 제 생각에는 객체지향에 대해서 좀 더 많이 고민해봐야 할 것 같아요

--- ClassInfoWriter.py
+++ ClassInfoWriter.py
@@ -5,3 +5,19 @@ def ClassInfoWriter():
     def __init__(self):
         self.f = open("./csv/"+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+'.csv','w', newline='')
         pass
+
+    def OpenFile(self, filePath, mode):
+        self.f = open("./csv/"+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+'.csv',mode, newline='')
+        
+        pass
+
+    def closeFile(self):
+        self.f.close()
+        pass
+
+    def Append(self):
+        self.OpenFile()
+
+
+
+        pass
