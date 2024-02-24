26345e7f7e26ae07849ce686f86faba3aecd30d9
ByeolNabi <20183338@office.deu.ac.kr>
2024-02-25 01:02:53 +0900

    수강신청 사이트에서 스크랩 하기 위한 틀 생성

--- README.md
+++ README.md
@@ -25,4 +25,3 @@ PW = "password"
 
 
 
-
---
+++ mainSugang.py
@@ -0,0 +1,26 @@
+import argparse
+from DAPscraper import *
+import Config
+
+
+def Parser():
+    # 입력된 id, pw를 파싱하고 값을 리턴합니다.
+    parser = argparse.ArgumentParser(description = "수강신청사이트 > 강의시간표 에 있는 수업계획서를 csv형태로 저장합니다.")
+    parser.add_argument('-u', '--username', type=str,required=True, help="DAP 아이디를 입력해주세요")
+    parser.add_argument('-p', '--password', type=str,required=True, help="DAP 비밀번호를 입력해주세요")
+    args = parser.parse_args()
+
+    return args
+
+def main():
+    # dict = Parser()
+    dict = {"username":Config.ID,"password":Config.PW}  # 디버깅을 위한 코드
+    print(dict)
+    scraper = DAPscraper()
+    scraper.Connect(dict)
+
+    pass
+
+if __name__ == "__main__":
+    # 계획중
+    pass
